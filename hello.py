76de99bd42f29ac516659c7ab6cb6fbee3bc1bcd
prabhakm <bhavyakm@gmail.com>
2017-05-05 12:31:50 -0400

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World from host \"%s\".\n' % socket.gethostname()
+
+@app.route('/prabha')
+def helloPrabha():
+        return "hello world prabha"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
